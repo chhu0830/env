efbe3833aa2108c97f36031761355df5a70e33e5
oHSUANo <chhu0830@gmail.com>
2016-07-17 16:40:26 +0800

    add export LC_ALL in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #/bin/sh
 
-apt-get install -y git zsh curl vim
+sudo apt-get install -y git zsh curl vim
 
-chsh $USER -s /bin/zsh
+sudo chsh $USER -s /bin/zsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-chmod +x install.sh
+sudo hmod +x install.sh
 ./install.sh
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp .zshrc ~/.zshrc
-chown $USER:$USER ~/.zshrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
-chown $USER:$USER ~/.vimrc
 vim +PluginInstall +qall
+
+export LC_ALL=en_US.UTF-8
