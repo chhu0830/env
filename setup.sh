472c65db86d4b4b368471411365c5e5fd08b3801
oHSUANo <chhu0830@gmail.com>
2016-07-30 20:34:41 +0800

    add .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
 sudo apt-get install -y git zsh curl vim
+cp .gitconfig ~/.gitconfig
 
 sudo chsh $USER -s /bin/zsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
