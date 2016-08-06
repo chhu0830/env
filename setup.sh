a3584b52cc7cd019852f12fc0419e23b3d99a379
oHSUANo <chhu0830@gmail.com>
2016-08-06 02:58:55 +0000

    fix small bug

--- setup.sh
+++ setup.sh
@@ -4,12 +4,11 @@ sudo apt-get install -y git zsh curl vim
 cp .gitconfig ~/.gitconfig
 
 sudo chsh $USER -s /bin/zsh
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sudo hmod +x install.sh
-./install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp .zshrc ~/.zshrc
+source ~/.zshrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
