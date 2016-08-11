e14084f2ce70b7fd3040eff6bccfbb1d821cec08
oHSUANo <chhu0830@gmail.com>
2016-08-11 15:53:15 +0800

    add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sudo apt-get install -y git zsh curl vim
 cp .gitconfig ~/.gitconfig
 
 sudo chsh $USER -s /bin/zsh
+export LC_ALL=en_US.UTF-8
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -14,4 +16,3 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
 
-export LC_ALL=en_US.UTF-8
