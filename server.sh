b9d8b9c070134e55a2c9bfdf9607ee163aeb48a0
oHSUANo <chhu0830@gmail.com>
2016-12-28 16:30:29 +0800

    Correct ln command

--- server.sh
+++ server.sh
@@ -2,23 +2,23 @@
 
 sudo apt-get install -y zsh vim tmux
 # git
-ln -s ./.gitconfig .gitconfig
+ln -s $PWD/.gitconfig .gitconfig
 
 # zsh
 chsh -s /bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-ln -s ./.zshrc .zshrc
+ln -s $PWD/.zshrc .zshrc
 source ~/.zshrc
 
 # vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s ./.vimrc ~/.vimrc
+ln -s $PWD/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s ./.tmux.conf ~/.tmux.conf
+ln -s $PWD/.tmux.conf ~/.tmux.conf
 tmux source ~/.tmux.conf
 
