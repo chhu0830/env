1d6f2d0aabed7f2008a28b3b4795892883d556f0
oHSUANo <chhu0830@gmail.com>
2017-06-19 08:59:24 +0800

    Revise the path of zsh auto-suggestion

--- server.sh
+++ server.sh
@@ -15,7 +15,7 @@ cp rkj-repos.zsh-theme ~/.oh-my-zsh/themes/
 rm ~/.zshrc
 ln -s $PWD/.zshrc ~/.zshrc
 source ~/.zshrc
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # vim
