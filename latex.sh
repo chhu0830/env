3e2e77b53583a44f89782633629c3a28c689ba81
oHSUANo <chhu0830@gmail.com>
2017-09-28 20:07:46 +0800

    Revise install command

--- latex.sh
+++ latex.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-sudo apt-get install -y texlive latex-cjk-all texlive-fonts-recommended latex-beamer texpower texlive-pictures texlive-latex-extra imagemagick
+sudo apt-get install -y texlive latex-cjk-all texlive-fonts-recommended texlive-latex-recommended texlive-pictures texlive-latex-extra imagemagick
