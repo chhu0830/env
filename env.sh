e4c4e85a1176ed5278f160da8c07f58d55be806a
oHSUANo <chhu0830@gmail.com>
2017-10-18 23:32:16 +0800

    Revise rm to rm -f to ignore error msg of file not exist

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y zsh vim tmux
 # git
-rm ~/.gitconfig
+rm -f ~/.gitconfig
 ln -s $PWD/.gitconfig ~/.gitconfig
 
 # ssh
@@ -11,21 +11,22 @@ ln -s $PWD/.gitconfig ~/.gitconfig
 # zsh
 chsh -s /bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp rkj-repos.zsh-theme ~/.oh-my-zsh/themes/
-rm ~/.zshrc
+mv ~/.oh-my-zsh/themes/rkj-repos.zsh-theme ~/.oh-my-zsh/themes/rkj-repos.zsh-theme.bak
+ln -s $PWD/rkj-repos.zsh-theme ~/.oh-my-zsh/themes/
+rm -f ~/.zshrc
 ln -s $PWD/.zshrc ~/.zshrc
-source ~/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm -f ~/.vimrc
 ln -s $PWD/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-rm ~/.tmux.conf
+rm -f ~/.tmux.conf
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 tmux source ~/.tmux.conf
 
