334af3eecbba277f4a1265edf358b6e11f9a95fa
oHSUANo <chhu0830@gmail.com>
2017-11-04 13:40:25 +0800

    Revise peda source

--- pwn.sh
+++ pwn.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 sudo apt-get install -y git gdb
-sudo git clone https://github.com/longld/peda.git /usr/local/peda
+sudo git clone https://github.com/scwuaptx/peda.git /usr/local/peda
 sudo git clone https://github.com/scwuaptx/Pwngdb /usr/local/Pwngdb
+cp /usr/local/peda/.inputrc ~/
 ln -s $PWD/.gdbinit ~/.gdbinit
 
 sudo apt-get install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
 sudo pip install --upgrade pip
 sudo pip install --upgrade pwntools
+sudo pip install --upgrade setuptools
 
 sudo apt-get install -y nmap
