b27e911f0772073717649b9593e8a8a83f60d2b0
oHSUANo <chhu0830@gmail.com>
2017-12-01 11:50:38 +0800

    Add angr & Revise the source of peda & Add one_gadget

--- pwn.sh
+++ pwn.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
 sudo apt-get install -y git gdb
-sudo git clone https://github.com/scwuaptx/peda.git /usr/local/peda
-sudo git clone https://github.com/scwuaptx/Pwngdb /usr/local/Pwngdb
-cp /usr/local/peda/.inputrc ~/
+git clone https://github.com/scwuaptx/peda.git ~/.peda
+git clone https://github.com/scwuaptx/Pwngdb ~/.Pwngdb
+cp ~/.peda/.inputrc ~/
 ln -s $PWD/.gdbinit ~/.gdbinit
 
 sudo apt-get install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
 sudo pip install --upgrade pip
-sudo pip install --upgrade setuptools
-sudo pip install --upgrade pwntools
-sudo pip install --upgrade ropgadget
+sudo pip install --upgrade setuptools ropgadget
+sudo pip insatll --upgrade pwntools angr
 
 sudo apt-get install -y nmap
+
+curl -sSL https://rvm.io/mpapis.asc | gpg --import
+curl -L https://get.rvm.io | bash -s stable --ruby
+zsh -c "gem install one_gadget"
