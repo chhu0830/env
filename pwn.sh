6d36bde1126d20bfc44c1e381e6359e8d63d2505
oHSUANo <chhu0830@gmail.com>
2017-12-05 14:41:15 +0800

    Revise apt-get to apt & Revise one_gadget installation

--- pwn.sh
+++ pwn.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-sudo apt-get install -y git gdb
+sudo apt install -y git gdb
 git clone https://github.com/scwuaptx/peda.git ~/.peda
 git clone https://github.com/scwuaptx/Pwngdb ~/.Pwngdb
 cp ~/.peda/.inputrc ~/
 ln -s $PWD/.gdbinit ~/.gdbinit
 
-sudo apt-get install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
+sudo apt install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
 sudo pip install --upgrade pip
 sudo pip install --upgrade setuptools ropgadget
 sudo pip insatll --upgrade pwntools angr
 
-sudo apt-get install -y nmap
+sudo apt install -y nmap
 
 curl -sSL https://rvm.io/mpapis.asc | gpg --import
 curl -L https://get.rvm.io | bash -s stable --ruby
-zsh -c "gem install one_gadget"
+zsh -c "source .zshrc && gem install one_gadget"
