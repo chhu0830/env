608aa9aec5bc915d9a2528a2dde68721838a4ad0
oHSUANo <chhu0830@gmail.com>
2017-12-16 16:19:06 +0800

    Add wine

--- env.sh
+++ env.sh
@@ -8,6 +8,9 @@ read -p "Install basic env? (Y/n)" yn
 read -p "Install i386 env? (Y/n)" yn
 [ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./i386.sh
 
+read -p "Install wine? (Y/n)" yn
+[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sudo apt install -y wine
+
 read -p "Install LAMP? (Y/n)" yn
 [ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./lamp.sh
 
