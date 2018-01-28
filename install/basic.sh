44e757ac3879e3cc64713768184161d4c3507aa9
oHSUANo <chhu0830@gmail.com>
2018-01-29 00:08:57 +0800

    Revise install to fetch config from github

--- install/basic.sh
+++ install/basic.sh
@@ -1,36 +1,44 @@
 #/bin/sh
-DIR=$PWD/config/basic
+URL="https://raw.githubusercontent.com/chhu0830/env/master/config/basic"
+DIR="$PWD/config/basic"
 
+mkdir -p $DIR
 sudo apt install -y git curl zsh vim tmux screen htop smbclient cifs-utils p7zip-full
 # git
-rm -f ~/.gitconfig
-ln -s $DIR/.gitconfig ~/.gitconfig
+rm -f $HOME/.gitconfig
+curl -fsSL $URL/.gitconfig > $DIR/.gitconfig
+ln -s $DIR/.gitconfig $HOME/.gitconfig
 
 # ssh
 mkdir ~/.ssh
-cp $DIR/.ssh.conf ~/.ssh/config
-sudo chmod 644 ~/.ssh/config
+curl -fsSL $URL/.ssh.conf > $DIR/.ssh.conf
+cp $DIR/.ssh.conf $HOME/.ssh/config
+sudo chmod 644 $HOME/.ssh/config
 
 # zsh
 sudo chsh -s /bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv ~/.oh-my-zsh/themes/rkj-repos.zsh-theme ~/.oh-my-zsh/themes/rkj-repos.zsh-theme.bak
-ln -s $DIR/rkj-repos.zsh-theme ~/.oh-my-zsh/themes/
-rm -f ~/.zshrc
-ln -s $DIR/.zshrc ~/.zshrc
+curl -fsSL $URL/zsh-repos.zsh-theme > $DIR/zsh-repos.zsh-theme
+mv $HOME/.oh-my-zsh/themes/rkj-repos.zsh-theme $HOME/.oh-my-zsh/themes/rkj-repos.zsh-theme.bak
+ln -s $DIR/rkj-repos.zsh-theme $HOME/.oh-my-zsh/themes/
+mv $HOME/.zshrc $HOME/.zshrc.bak
+curl -fsSL $URL/.zshrc > $DIR/.zshrc
+ln -s $DIR/.zshrc $HOME/.zshrc
 git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-rm -f ~/.vimrc
+mv $HOME/.vimrc $HOME/.vimrc.bak
+curl -fsSL $URL/.vimrc > $DIR/.vimrc
 ln -s $DIR/.vimrc ~/.vimrc
 vim +PlugInstall
 sudo apt install silversearcher-ag
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-rm -f ~/.tmux.conf
-ln -s $DIR/.tmux.conf ~/.tmux.conf
+curl -fsSL $URL/.tmux.conf > $DIR/.tmux.conf
+mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+ln -s $DIR/.tmux.conf $HOME/.tmux.conf
 # tmux source ~/.tmux.conf
 
--- install/env.sh
+++ install/env.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+URL="https://raw.githubusercontent.com/chhu0830/env/master/install"
+DIR="$HOME/.env"
 
 sudo apt update
 
-read -p "Install basic env? (Y/n)" yn
-[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./basic.sh
+read -p "Where do you want to put your config file? ($DIR) " dir
+[ "${dir}" != "" ] && DIR=${dir}
+mkdir -p $DIR
+cd $DIR
 
-read -p "Install i386 env? (Y/n)" yn
-[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./i386.sh
+read -p "Install basic env? (Y/n) " yn
+[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sh -c "$(curl -fsSL $URL/basic.sh)"
 
-read -p "Install wine? (Y/n)" yn
-[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sudo apt install -y wine
+read -p "Install i386 env? (Y/n) " yn
+[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sh -c "$(curl -fsSL $URL/i386.sh)"
 
-read -p "Install LaTeX? (Y/n)" yn
-[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./latex.sh
+read -p "Install LaTeX? (Y/n) " yn
+[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sh -c "$(curl -fsSL $URL/latex.sh)"
 
-read -p "Install LAMP? (Y/n)" yn
-[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./lamp.sh
+read -p "Install LAMP? (Y/n) " yn
+[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sh -c "$(curl -fsSL $URL/lamp.sh)"
 
-read -p "Install pwn env? (Y/n)" yn
-[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && ./pwn.sh
+read -p "Install pwn env? (Y/n) " yn
+[ "${yn}" == "Y" -o "${yn}" == "y" -o "${yn}" == "" ] && sh -c "$(curl -fsSL $URL/pwn.sh)"
