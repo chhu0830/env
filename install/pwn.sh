43dbd61ef42589207abc45788518be114dd9e041
oHSUANo <chhu0830@gmail.com>
2018-01-29 00:57:48 +0800

    Revise install to fetch config from github

--- install/pwn.sh
+++ install/pwn.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+URL="https://raw.githubusercontent.com/chhu0830/env/master/config/pwn"
 DIR=$PWD/config/pwn
 
 sudo apt install -y git gdb
 git clone https://github.com/scwuaptx/peda.git ~/.peda
 git clone https://github.com/scwuaptx/Pwngdb ~/.Pwngdb
-cp ~/.peda/.inputrc ~/
-ln -s $DIR/.gdbinit ~/.gdbinit
+cp $HOME/.peda/.inputrc $HOME/
+curl -fsSL $URL/.gdbinit > $DIR/.gdbinit
+ln -s $DIR/.gdbinit $HOME/.gdbinit
 
 sudo apt install -y python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
 sudo pip install --upgrade pip
@@ -19,25 +21,27 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 zsh -c "source .zshrc && gem install one_gadget"
 
 git clone https://github.com/guelfoweb/knock.git ~/.knock
-mv ~/.knock/knockpy/knockpy.py ~/.knock/knockpy/knockpy
-sudo chmod +x ~/.knock/knockpy/knockpy
+mv $HOME/.knock/knockpy/knockpy.py $HOME/.knock/knockpy/knockpy
+sudo chmod +x $HOME/.knock/knockpy/knockpy
 
 wget -c 'http://sourceforge.net/projects/dirb/files/dirb/2.03/dirb203.tar.gz/download' -O dirb203.tar.gz
 tar -zxvf dirb203.tar.gz
 rm dirb203.tar.gz
 cd dirb
-sudo apt install libcurl4-gnutls-dev
+sudo apt install -y libcurl4-gnutls-dev
 ./configure
 make
 sudo make install
 cd ..
 sudo mv -f dirb /usr/share/dirb
 
-sudo apt install build-essential python-pip libyaml-dev python-dev
+sudo apt install -y build-essential python-pip libyaml-dev python-dev
 sudo pip install --upgrade prettytable Mako PyYAML python-dateutil PySocks
 git clone https://github.com/epinna/weevely3.git ~/.weevely
-mv ~/.weevely/weevely.py ~/.weevely/weevely
+mv $HOME/.weevely/weevely.py $HOME/.weevely/weevely
 sudo chmod +x ~/.weevely/weevely
 
-sudo apt install openjdk-8-jre
+sudo apt install -y openjdk-8-jre
+curl -fsSL "https://raw.githubusercontent.com/chhu0830/env/master/install/burpsuite.sh" > burpsuit.sh
 ./burpsuite.sh
+rm burpsuit.sh
