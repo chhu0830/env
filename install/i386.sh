3c6e5b14aa85f3327167f82f5a2b78c48ac77c34
oHSUANo <chhu0830@gmail.com>
2018-08-20 09:45:47 +0800

    Add support for i386 program running on x86-64 machine

--- install/i386.sh
+++ install/i386.sh
@@ -2,4 +2,4 @@
 
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386
+sudo apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386 libc6-dev-i386
