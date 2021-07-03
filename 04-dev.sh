a0defd3f135373fae6ec8151e2ee3d3e1f70fbf6
Chia-Hsuan Hu <chhu0830@gmail.com>
2021-07-03 14:35:07 +0800

    refactor: use here-document to write config

--- 04-dev.sh
+++ 04-dev.sh
@@ -32,19 +32,24 @@ sudo $INS $INSOPT \
   make build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo pip3 install virtualenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-echo 'export PYENV_ROOT="${HOME}/.pyenv"' >> ${HOME}/.zshenv
-echo 'export PATH="${PYENV_ROOT}/bin:${PATH}"' >> ${HOME}/.zshenv
-echo 'eval "$(pyenv init --path)"' >> ${HOME}/.zshenv
-echo 'eval "$(pyenv init -)"' >> ${HOME}/.zshenv
-echo 'eval "$(pyenv virtualenv-init -)"' >> ${HOME}/.zshenv
+cat >> ${HOME}/.zshenv <<EOF
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
 
 
 ### goenv ###
 sudo $INS $INSOPT golang
 git clone https://github.com/syndbg/goenv.git ~/.goenv
-echo 'export GOENV_ROOT="${HOME}/.goenv"' >> ${HOME}/.zshenv
-echo 'export PATH="${GOENV_ROOT}/bin:$PATH"' >> ${HOME}/.zshenv
-echo 'eval "$(goenv init -)"' >> ${HOME}/.zshenv
-# echo 'export PATH="${GOROOT}/bin:${PATH}"' >> ${HOME}/.zshenv
-echo 'export PATH="${PATH}:${GOPATH}/bin"' >> ${HOME}/.zshenv
+cat >> ${HOME}/.zshenv <<EOF
+export GOENV_ROOT="${HOME}/.goenv"
+export PATH="${GOENV_ROOT}/bin:$PATH"
+eval "$(goenv init -)"
+export PATH="${GOROOT:+${GOROOT}/bin:}${PATH}"
+export PATH="${PATH}${GOPATH:+:${GOPATH}/bin}"
+EOF
